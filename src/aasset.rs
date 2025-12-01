1b2b375b57a8ce9b669f0b15f7e693288c7a89ae
mcbegamerxx954 <154642722+mcbegamerxx954@users.noreply.github.com>
2025-12-01 14:36:32 -0300

    Test of ghost assets

--- src/aasset.rs
+++ src/aasset.rs
@@ -5,6 +5,7 @@ use crate::{
     cpp_string::{ResourceLocation, StackString},
     jniopts::OPTS,
 };
+use asset_overlay::{FileProvider, SyncFile};
 use cxx::CxxString;
 use libc::{c_char, c_int, c_void, off64_t, off_t, size_t};
 use materialbin::{
@@ -29,22 +30,10 @@ use std::{
         LazyLock, Mutex, OnceLock,
     },
 };
-static MC_FILELOADER: LazyLock<Mutex<FileLoader>> =
-    LazyLock::new(|| Mutex::new(FileLoader { last_buffer: None }));
-// This makes me feel wrong... but all we will do is compare the pointer
-// and the struct will be used in a mutex so this is safe??
-#[derive(PartialEq, Eq, Hash)]
-struct AAssetPtr(*const ndk_sys::AAsset);
-unsafe impl Send for AAssetPtr {}
-
 // The Minecraft version we will use to port shaders to
 static MC_VERSION: OnceLock<Option<MinecraftVersion>> = OnceLock::new();
 static IS_1_21_100: AtomicBool = AtomicBool::new(false);
-// The assets we have registered to replace data about
-static mut WANTED_ASSETS: Lazy<UnsafeCell<HashMap<AAssetPtr, Buffer>>> =
-    Lazy::new(|| UnsafeCell::new(HashMap::new()));
-
-fn get_current_mcver(man: ndk::asset::AssetManager) -> Option<MinecraftVersion> {
+fn get_current_mcver(man: &AssetManager) -> Option<MinecraftVersion> {
     let mut file = match get_uitext(man) {
         Some(asset) => asset,
         None => {
@@ -73,7 +62,7 @@ fn get_current_mcver(man: ndk::asset::AssetManager) -> Option<MinecraftVersion> 
 }
 
 // Try to open UIText.material.bin to guess Minecraft shader version
-fn get_uitext(man: ndk::asset::AssetManager) -> Option<Asset> {
+fn get_uitext(man: &AssetManager) -> Option<Asset> {
     const NEW: &CStr = c"assets/renderer/materials/RenderChunk.material.bin";
     const OLD: &CStr = c"renderer/materials/RenderChunk.material.bin";
     for path in [NEW, OLD] {
@@ -92,42 +81,6 @@ macro_rules! folder_list {
         ]
     }
 }
-pub unsafe extern "C" fn open(
-    man: *mut AAssetManager,
-    fname: *const c_char,
-    mode: c_int,
-) -> *mut AAsset {
-    // This is where UB can happen, but we are merely a hook.
-    let aasset = unsafe { ndk_sys::AAssetManager_open(man, fname, mode) };
-    let pointer = match std::ptr::NonNull::new(man) {
-        Some(yay) => yay,
-        None => {
-            log::warn!("AssetManager is null?, preposterous, mc detection failed");
-            return aasset;
-        }
-    };
-    let manager = unsafe { ndk::asset::AssetManager::from_ptr(pointer) };
-    let c_str = unsafe { CStr::from_ptr(fname) };
-    let raw_cstr = c_str.to_bytes();
-    let os_str = OsStr::from_bytes(raw_cstr);
-    let c_path: &Path = Path::new(os_str);
-    let mut sus = MC_FILELOADER.lock().unwrap();
-    if let Some(yay) = sus.get_file(c_path, manager) {
-        WANTED_ASSETS.get_mut().insert(AAssetPtr(aasset), yay);
-    }
-    return aasset;
-}
-macro_rules! handle_result {
-    ($expr:expr) => {
-        match $expr {
-            Ok(val) => val,
-            Err(e) => {
-                log::error!("{e}");
-                return -1;
-            }
-        }
-    };
-}
 // This lint is not really applicable
 #[allow(clippy::unused_io_amount)]
 /// Join paths directly into a c++ string
@@ -142,7 +95,7 @@ fn opt_path_join(mut bytes: Pin<&mut CxxString>, paths: &[&Path]) {
             .expect("Error while writing path to stack path");
     }
 }
-fn process_material(man: AssetManager, data: &[u8]) -> Option<Vec<u8>> {
+fn process_material(man: &AssetManager, data: &[u8]) -> Option<Vec<u8>> {
     let mcver = MC_VERSION.get_or_init(|| get_current_mcver(man));
     // Just ignore if no Minecraft version was found
     let mcver = (*mcver)?;
@@ -258,168 +211,6 @@ fn replace_bytes(codebuf: &mut Vec<u8>, finder: &Finder, pattern: &[u8], replace
     };
     codebuf.splice(sus..sus + pattern.len(), replace_with.iter().cloned());
 }
-pub unsafe extern "C" fn seek64(aasset: *mut AAsset, off: off64_t, whence: c_int) -> off64_t {
-    let file = match WANTED_ASSETS.get_mut().get_mut(&AAssetPtr(aasset)) {
-        Some(file) => file,
-        None => return ndk_sys::AAsset_seek64(aasset, off, whence),
-    };
-    handle_result!(seek_facade(off, whence, file).try_into())
-}
-
-pub unsafe extern "C" fn seek(aasset: *mut AAsset, off: off_t, whence: c_int) -> off_t {
-    let wanted_assets = WANTED_ASSETS.get_mut();
-    let file = match wanted_assets.get_mut(&AAssetPtr(aasset)) {
-        Some(file) => file,
-        None => return ndk_sys::AAsset_seek(aasset, off, whence),
-    };
-    handle_result!(seek_facade(off.into(), whence, file).try_into())
-}
-
-pub unsafe extern "C" fn read(aasset: *mut AAsset, buf: *mut c_void, count: size_t) -> c_int {
-    let wanted_assets = WANTED_ASSETS.get_mut();
-    let file = match wanted_assets.get_mut(&AAssetPtr(aasset)) {
-        Some(file) => file,
-        None => return ndk_sys::AAsset_read(aasset, buf, count),
-    };
-    // Reuse buffer given by caller
-    let rs_buffer = core::slice::from_raw_parts_mut(buf as *mut u8, count);
-    let read_total = match (*file).read(rs_buffer) {
-        Ok(n) => n,
-        Err(e) => {
-            log::warn!("failed fake aaset read: {e}");
-            return -1 as c_int;
-        }
-    };
-    handle_result!(read_total.try_into())
-}
-
-pub unsafe extern "C" fn len(aasset: *mut AAsset) -> off_t {
-    let wanted_assets = WANTED_ASSETS.get_mut();
-    let file = match wanted_assets.get(&AAssetPtr(aasset)) {
-        Some(file) => file,
-        None => return ndk_sys::AAsset_getLength(aasset),
-    };
-    handle_result!(file.get_ref().len().try_into())
-}
-
-pub unsafe extern "C" fn len64(aasset: *mut AAsset) -> off64_t {
-    let wanted_assets = WANTED_ASSETS.get_mut();
-    let file = match wanted_assets.get(&AAssetPtr(aasset)) {
-        Some(file) => file,
-        None => return ndk_sys::AAsset_getLength64(aasset),
-    };
-    handle_result!(file.get_ref().len().try_into())
-}
-
-pub unsafe extern "C" fn rem(aasset: *mut AAsset) -> off_t {
-    let wanted_assets = WANTED_ASSETS.get_mut();
-    let file = match wanted_assets.get(&AAssetPtr(aasset)) {
-        Some(file) => file,
-        None => return ndk_sys::AAsset_getRemainingLength(aasset),
-    };
-    handle_result!((file.get_ref().len() - file.position() as usize).try_into())
-}
-
-pub unsafe extern "C" fn rem64(aasset: *mut AAsset) -> off64_t {
-    let wanted_assets = WANTED_ASSETS.get_mut();
-    let file = match wanted_assets.get(&AAssetPtr(aasset)) {
-        Some(file) => file,
-        None => return ndk_sys::AAsset_getRemainingLength64(aasset),
-    };
-    handle_result!((file.get_ref().len() - file.position() as usize).try_into())
-}
-
-pub unsafe extern "C" fn close(aasset: *mut AAsset) {
-    let wanted_assets = WANTED_ASSETS.get_mut();
-    if let Some(buffer) = wanted_assets.remove(&AAssetPtr(aasset)) {
-        MC_FILELOADER.lock().unwrap().last_buffer = Some(buffer);
-    }
-    ndk_sys::AAsset_close(aasset);
-}
-
-pub unsafe extern "C" fn get_buffer(aasset: *mut AAsset) -> *const c_void {
-    let wanted_assets = WANTED_ASSETS.get_mut();
-    let file = match wanted_assets.get_mut(&AAssetPtr(aasset)) {
-        Some(file) => file,
-        None => return ndk_sys::AAsset_getBuffer(aasset),
-    };
-    // Let's hope this does not go boom boom
-    file.get_ref().as_ptr().cast()
-}
-
-pub unsafe extern "C" fn fd_dummy(
-    aasset: *mut AAsset,
-    out_start: *mut off_t,
-    out_len: *mut off_t,
-) -> c_int {
-    let wanted_assets = WANTED_ASSETS.get_mut();
-    match wanted_assets.get(&AAssetPtr(aasset)) {
-        Some(_) => {
-            log::error!("WE GOT BUSTED NOOO");
-            -1
-        }
-        None => ndk_sys::AAsset_openFileDescriptor(aasset, out_start, out_len),
-    }
-}
-
-pub unsafe extern "C" fn fd_dummy64(
-    aasset: *mut AAsset,
-    out_start: *mut off64_t,
-    out_len: *mut off64_t,
-) -> c_int {
-    let wanted_assets = WANTED_ASSETS.get_mut();
-    match wanted_assets.get(&AAssetPtr(aasset)) {
-        Some(_) => {
-            log::error!("WE GOT BUSTED NOOO");
-            -1
-        }
-        None => ndk_sys::AAsset_openFileDescriptor64(aasset, out_start, out_len),
-    }
-}
-
-pub unsafe extern "C" fn is_alloc(aasset: *mut AAsset) -> c_int {
-    let wanted_assets = WANTED_ASSETS.get_mut();
-    match wanted_assets.get(&AAssetPtr(aasset)) {
-        Some(_) => false as c_int,
-        None => ndk_sys::AAsset_isAllocated(aasset),
-    }
-}
-
-fn seek_facade(offset: i64, whence: c_int, file: &mut Buffer) -> i64 {
-    let offset = match whence {
-        libc::SEEK_SET => {
-            //Let's check this so we don't mess up
-            let u64_off = match u64::try_from(offset) {
-                Ok(uoff) => uoff,
-                Err(e) => {
-                    log::error!("signed ({offset}) to unsigned failed: {e}");
-                    return -1;
-                }
-            };
-            io::SeekFrom::Start(u64_off)
-        }
-        libc::SEEK_CUR => io::SeekFrom::Current(offset),
-        libc::SEEK_END => io::SeekFrom::End(offset),
-        _ => {
-            log::error!("Invalid seek whence");
-            return -1;
-        }
-    };
-    match file.seek(offset) {
-        Ok(new_offset) => match new_offset.try_into() {
-            Ok(int) => int,
-            Err(err) => {
-                log::error!("u64 ({new_offset}) to i64 failed: {err}");
-                -1
-            }
-        },
-        Err(err) => {
-            log::error!("aasset seek failed: {err}");
-            -1
-        }
-    }
-}
-
 enum BufferCursor {
     Vec(Cursor<Vec<u8>>),
     Cxx(Cursor<StackString>),
@@ -454,18 +245,10 @@ impl BufferCursor {
         }
     }
 }
-
-struct FileLoader {
-    last_buffer: Option<Buffer>,
-}
-impl FileLoader {
-    fn get_file(&mut self, path: &Path, manager: AssetManager) -> Option<Buffer> {
+pub struct Mbl2Backend {}
+impl FileProvider for Mbl2Backend {
+    fn get_file(&mut self, path: &Path, manager: &AssetManager) -> Option<Box<SyncFile>> {
         let stripped = path.strip_prefix("assets/").unwrap_or(path);
-        if let Some(mut cache) = self.last_buffer.take_if(|c| c.name == path) {
-            log::info!("Cache hit!: {:#?}", path);
-            cache.rewind().unwrap();
-            return Some(cache);
-        }
         let replacement_list = folder_list! {
             apk: "gui/dist/hbui/" -> pack: "hbui/",
             apk: "skin_packs/persona/" -> pack: "persona/",
@@ -502,16 +285,16 @@ impl FileLoader {
                     .as_encoded_bytes()
                     .ends_with(b".material.bin")
                 {
-                    match process_material(manager, cxx_ptr.as_bytes()) {
+                    match process_material(&manager, cxx_ptr.as_bytes()) {
                         Some(updated) => BufferCursor::Vec(Cursor::new(updated)),
                         None => BufferCursor::Cxx(Cursor::new(cxx_storage)),
                     }
                 } else {
                     BufferCursor::Cxx(Cursor::new(cxx_storage))
                 };
-                let cache = Buffer::new(path.to_path_buf(), buffer);
+                //                let cache = Buffer::new(path.to_path_buf(), buffer);
                 // ResourceLocation gets dropped (also cxx_storage if its not needed)
-                return Some(cache);
+                return Some(Box::new(buffer));
             }
         }
         None
