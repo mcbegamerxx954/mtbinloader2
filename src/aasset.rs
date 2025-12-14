83e744577cd64ffcd51ba8497fb960db7ff2d74b
mcbegamerxx954 <154642722+mcbegamerxx954@users.noreply.github.com>
2025-12-14 14:42:52 -0300

    Split up the codebase

--- src/aasset.rs
+++ src/aasset.rs
@@ -1,107 +1,35 @@
 //Explanation: Aasset is NOT thread-safe anyways so we will not try adding thread safety either
 #![allow(static_mut_refs)]
-
 use crate::{
-    cpp_string::{ResourceLocation, StackString},
-    jniopts::OPTS,
+    loader::{Buffer, FileLoader},
     LockResultExt,
 };
-use cxx::CxxString;
 use libc::{c_char, c_int, c_void, off64_t, off_t, size_t};
-use materialbin::{
-    bgfx_shader::BgfxShader,
-    pass::{ShaderCodePlatform, ShaderStage},
-    CompiledMaterialDefinition, MinecraftVersion,
-};
-use memchr::memmem::Finder;
-use ndk::asset::{Asset, AssetManager};
+
 use ndk_sys::{AAsset, AAssetManager};
 use once_cell::sync::Lazy;
-use scroll::Pread;
 use std::{
     cell::UnsafeCell,
     collections::HashMap,
     error::Error,
     ffi::{CStr, OsStr},
-    io::{self, Cursor, Read, Seek, Write},
-    ops::{Deref, DerefMut},
+    io::{self, Read, Seek},
     os::unix::ffi::OsStrExt,
-    path::{Path, PathBuf},
-    pin::Pin,
+    path::Path,
     ptr,
-    sync::{
-        atomic::{AtomicBool, Ordering},
-        LazyLock, Mutex, OnceLock,
-    },
+    sync::{LazyLock, Mutex},
 };
-static MC_FILELOADER: LazyLock<Mutex<FileLoader>> =
-    LazyLock::new(|| Mutex::new(FileLoader { last_buffer: None }));
+static MC_FILELOADER: LazyLock<Mutex<FileLoader>> = LazyLock::new(|| Mutex::new(FileLoader::new()));
 // This makes me feel wrong... but all we will do is compare the pointer
 // and the struct will be used in a mutex so this is safe??
 #[derive(PartialEq, Eq, Hash)]
 struct AAssetPtr(*const ndk_sys::AAsset);
 unsafe impl Send for AAssetPtr {}
 
-// The Minecraft version we will use to port shaders to
-static MC_VERSION: OnceLock<Option<MinecraftVersion>> = OnceLock::new();
-static MC_IS_1_21_100: AtomicBool = AtomicBool::new(false);
-static MC_IS_1_21_130: AtomicBool = AtomicBool::new(false);
 // The assets we have registered to replace data about
 static mut WANTED_ASSETS: Lazy<UnsafeCell<HashMap<AAssetPtr, Buffer>>> =
     Lazy::new(|| UnsafeCell::new(HashMap::new()));
 
-fn get_current_mcver(man: ndk::asset::AssetManager) -> Option<MinecraftVersion> {
-    let mut file = match get_uitext(man) {
-        Some(asset) => asset,
-        None => {
-            log::error!("Shader fixing is disabled as RenderChunk was not found");
-            return None;
-        }
-    };
-    let mut buf = Vec::with_capacity(file.length());
-    if let Err(e) = file.read_to_end(&mut buf) {
-        log::error!("Something is wrong with AssetManager, mc detection failed: {e}");
-        return None;
-    };
-
-    for version in materialbin::ALL_VERSIONS.into_iter().rev() {
-        if let Ok(_shader) = buf.pread_with::<CompiledMaterialDefinition>(0, version) {
-            log::info!("Mc version is {version}");
-            if memchr::memmem::find(&buf, b"v_dithering").is_some() {
-                MC_IS_1_21_100.store(true, Ordering::Release);
-                log::info!("Mc version is 1_21_100 or higher");
-            } // else { panic!("mc version unsupported! cannot find:v_dithering"); };
-            if memchr::memmem::find(&buf, b"a_texcoord1 * 65535.0").is_some() {
-                MC_IS_1_21_130.store(true, Ordering::Release);
-                log::info!("Mc version is 1_21_130 or higher");
-            } // else { panic!("mc version unsupported! cannot find:a_texcoord1*65535.0"); };
-            return Some(version);
-        };
-    }
-    log::warn!("Cannot detect mc version, autofix disabled");
-    None
-}
-
-// Try to open UIText.material.bin to guess Minecraft shader version
-fn get_uitext(man: ndk::asset::AssetManager) -> Option<Asset> {
-    const NEW: &CStr = c"assets/renderer/materials/RenderChunk.material.bin";
-    const OLD: &CStr = c"renderer/materials/RenderChunk.material.bin";
-    for path in [NEW, OLD] {
-        if let Some(asset) = man.open(path) {
-            return Some(asset);
-        }
-    }
-    None
-}
-macro_rules! folder_list {
-    ($( apk: $apk_folder:literal -> pack: $pack_folder:expr),
-        *,
-    ) => {
-        [
-            $(($apk_folder, $pack_folder)),*,
-        ]
-    }
-}
 pub unsafe extern "C" fn open(
     man: *mut AAssetManager,
     fname: *const c_char,
@@ -138,216 +66,7 @@ macro_rules! handle_result {
         }
     };
 }
-// This lint is not really applicable
-#[allow(clippy::unused_io_amount)]
-/// Join paths directly into a c++ string
-fn opt_path_join(mut bytes: Pin<&mut CxxString>, paths: &[&Path]) {
-    let total_len: usize = paths.iter().map(|p| p.as_os_str().len()).sum();
-    bytes.as_mut().reserve(total_len);
-    let mut writer = bytes;
-    for path in paths {
-        let osstr = path.as_os_str().as_bytes();
-        writer
-            .write(osstr)
-            .expect("Error while writing path to stack path");
-    }
-}
-fn process_material(man: AssetManager, data: &[u8]) -> Option<Vec<u8>> {
-    let mcver = MC_VERSION.get_or_init(|| get_current_mcver(man));
-    // Just ignore if no Minecraft version was found
-    let mcver = (*mcver)?;
-    let opts = OPTS.lock().ignore_poison();
-    for version in opts.autofixer_versions.iter() {
-        let version = *version;
-        let mut material: CompiledMaterialDefinition = match data.pread_with(0, version) {
-            Ok(data) => data,
-            Err(e) => {
-                log::trace!("[version] Parsing failed: {e}");
-                continue;
-            }
-        };
-        let needs_lightmap_fix = MC_IS_1_21_100.load(Ordering::Acquire)
-            // && version != MinecraftVersion::V1_21_110
-            && (material.name == "RenderChunk" || material.name == "RenderChunkPrepass")
-            && opts.handle_lightmaps;
-        let needs_sampler_fix = material.name == "RenderChunk"
-            && mcver >= MinecraftVersion::V1_20_80
-            && version <= MinecraftVersion::V1_19_60
-            && opts.handle_texturelods;
-        // Prevent some work
-        if version == mcver && !needs_lightmap_fix && !needs_sampler_fix {
-            log::info!("Did not fix mtbin, mtversion: {version}");
-            return None;
-        }
-        let mut changed = 0;
-        if needs_lightmap_fix {
-            log::warn!("Had to fix lightmaps for RenderChunk");
-            handle_lightmaps(&mut material, version, &mut changed);
-            log::warn!("autofix have changed {changed} passes");
-            if changed == 0 {
-                log::info!("nothing changed, skip writting");
-                return None; //shader is already 1.21.100+
-            }
-        }
-        if needs_sampler_fix {
-            log::warn!("Had to fix mipmap levels for RenderChunk");
-            handle_samplers(&mut material);
-        }
-        let mut output = Vec::with_capacity(data.len());
-        if let Err(e) = material.write(&mut output, mcver) {
-            log::trace!("[version] Write error: {e}");
-            return None;
-        }
-        return Some(output);
-    }
 
-    None
-}
-fn handle_lightmaps(
-    materialbin: &mut CompiledMaterialDefinition,
-    version: MinecraftVersion,
-    changed: &mut i32,
-) {
-    //log::info!("mtbinloader25 handle_lightmaps");
-    let pattern = b"void main";
-    //     let replace_with = b"
-    // #define a_texcoord1 vec2(fract(a_texcoord1.x*15.9375)+0.0001,floor(a_texcoord1.x*15.9375)*0.0625+0.0001)
-    // void main";
-    let mut replace_with: &[u8] = b"void main";
-    let lightmap_10023_11020: &[u8] = b"
-vec2 lightmapUtil_10023_11020_ead63a(vec2 tc1){
-    return clamp(vec2(uvec2(
-        uint(floor(tc1.x * 255.0)) & 15u,
-        uint(floor(tc1.x * 255.0)) >> 4u
-    ) & 15u) * 0.0625, 0.0, 1.0);
-}
-#ifdef a_texcoord1
- #undef a_texcoord1
-#endif
-#define a_texcoord1 lightmapUtil_10023_11020_ead63a(a_texcoord1)
-void main";
-    let lightmap_10023_13028: &[u8] = b"
-vec2 lightmapUtil_10023_13028_190d99(vec2 tc1){
-    return clamp(vec2(uvec2(
-        uint(round(tc1.y * 65535.0)) >> 4u,
-        uint(round(tc1.y * 65535.0)) & 15u
-    ) & 15u) * 0.066666, 0.0, 1.0);
-}
-#ifdef a_texcoord1
- #undef a_texcoord1
-#endif
-#define a_texcoord1 lightmapUtil_10023_13028_190d99(a_texcoord1)
-void main";
-    let lightmap_11020_13028: &[u8] = b"
-vec2 lightmapUtil_11020_13028_274db2(vec2 tc1){
-    uvec2 uv = uvec2(
-        uint(round(tc1.y * 65535.0)) >> 4u,
-        uint(round(tc1.y * 65535.0)) & 15u
-    ) & 15u;
-    return vec2(float((uv.y << 4u) | uv.x) / 255.0, 0.0);
-}
-#ifdef a_texcoord1
- #undef a_texcoord1
-#endif
-#define a_texcoord1 lightmapUtil_11020_13028_274db2(a_texcoord1)
-void main";
-    let finder = Finder::new(pattern);
-    let finder1 = Finder::new(b"v_lightmapUV = a_texcoord1;");
-    let finder2 = Finder::new(b"v_lightmapUV=a_texcoord1;");
-    //let finder3 = Finder::new(b"#define a_texcoord1 ");
-    let finder4 = Finder::new(b"65535.0");
-    for (_, code) in materialbin
-        .passes
-        .iter_mut()
-        .flat_map(|(_, pass)| &mut pass.variants)
-        .flat_map(|variants| &mut variants.shader_codes)
-        .filter(|(stage, _)| {
-            stage.stage == ShaderStage::Vertex
-                && (stage.platform == ShaderCodePlatform::Essl100
-                    || stage.platform == ShaderCodePlatform::Essl300)
-        })
-    {
-        // if version == MinecraftVersion::V1_21_20
-        // if stage.platform == ShaderCodePlatform::Essl100
-        // if stage.platform_name != "ESSL_310" && (
-        // if version != MinecraftVersion::V1_21_110
-        // log::warn!("Skipping replacement due to not existing lightmap UV assignment");
-        // let mut bgfx: BgfxShader = code.bgfx_shader_data.pread(0).unwrap();
-        let blob = &mut code.bgfx_shader_data;
-        let Ok(mut bgfx) = blob.pread::<BgfxShader>(0) else {
-            continue;
-        };
-        let mc_1_21_130 = MC_IS_1_21_130.load(Ordering::Acquire);
-        // shader is 1-21-100 or above
-        if finder1.find(&bgfx.code).is_none() && finder2.find(&bgfx.code).is_none() {
-            if version >= MinecraftVersion::V1_21_110 && finder4.find(&bgfx.code).is_some() {
-                //shader is already 1-21-130
-                log::info!("finder already 1_21_130!!! Skipping replacement...");
-                continue;
-            } else if mc_1_21_130 {
-                log::info!("autofix: 11020 -> 13028");
-                replace_with = lightmap_11020_13028;
-            } else {
-                log::info!("finder already 1_21_110!!! Skipping replacement...");
-                continue;
-            }
-        } else if mc_1_21_130 {
-            log::info!("autofix: 10023 -> 13028");
-            replace_with = lightmap_10023_13028;
-        } else {
-            log::info!("autofix: 10023 -> 11020");
-            replace_with = lightmap_10023_11020;
-        }
-        *changed += 1;
-        //log::info!("autofix is doing lightmap replacing...");
-        replace_bytes(&mut bgfx.code, &finder, pattern, replace_with);
-        blob.clear();
-        let _unused = bgfx.write(blob);
-    }
-}
-// fn cmp_ign_whitespace(str1: &str, str2: &str) -> bool {
-//     str1.chars().filter(|c| !c.is_whitespace()).eq(str2.chars())
-// }
-fn handle_samplers(materialbin: &mut CompiledMaterialDefinition) {
-    //log::info!("mtbinloader25 handle_samplers");
-    let pattern = b"void main ()";
-    let replace_with: &[u8] = b"
-#if __VERSION__ >= 300
- #define texture(tex,uv) textureLod(tex,uv,0.0)
-#else
- #define texture2D(tex,uv) texture2DLod(tex,uv,0.0)
-#endif
-void main ()";
-    let finder = Finder::new(pattern);
-    for (_, code) in materialbin
-        .passes
-        .iter_mut()
-        .filter(|(passes, _)| *passes == "AlphaTest" || *passes == "Opaque")
-        .flat_map(|(_, pass)| &mut pass.variants)
-        .flat_map(|variants| &mut variants.shader_codes)
-        .filter(|(stage, _)| {
-            stage.stage == ShaderStage::Fragment && stage.platform_name == "ESSL_100"
-        })
-    {
-        log::info!("handling texture sampler to disable mipmap...");
-        let mut bgfx: BgfxShader = code
-            .bgfx_shader_data
-            .pread(0)
-            .expect("Failed reading bgfx shader data");
-        replace_bytes(&mut bgfx.code, &finder, pattern, replace_with);
-        code.bgfx_shader_data.clear();
-        bgfx.write(&mut code.bgfx_shader_data)
-            .expect("bgfx shader Write error... huh");
-    }
-}
-
-fn replace_bytes(codebuf: &mut Vec<u8>, finder: &Finder, pattern: &[u8], replace_with: &[u8]) {
-    let sus = match finder.find(codebuf) {
-        Some(yay) => yay,
-        None => return,
-    };
-    codebuf.splice(sus..sus + pattern.len(), replace_with.iter().cloned());
-}
 pub unsafe extern "C" fn seek64(aasset: *mut AAsset, off: off64_t, whence: c_int) -> off64_t {
     let file = match WANTED_ASSETS.get_mut().get_mut(&AAssetPtr(aasset)) {
         Some(file) => file,
@@ -498,125 +217,6 @@ fn seek_facade(offset: i64, whence: c_int, file: &mut Buffer) -> i64 {
     }
 }
 
-enum BufferCursor {
-    Vec(Cursor<Vec<u8>>),
-    Cxx(Cursor<StackString>),
-}
-impl Read for BufferCursor {
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        match self {
-            Self::Vec(v) => v.read(buf),
-            Self::Cxx(cxx) => cxx.read(buf),
-        }
-    }
-}
-impl Seek for BufferCursor {
-    fn seek(&mut self, pos: io::SeekFrom) -> io::Result<u64> {
-        match self {
-            Self::Vec(v) => v.seek(pos),
-            Self::Cxx(cxx) => cxx.seek(pos),
-        }
-    }
-}
-impl BufferCursor {
-    fn position(&self) -> u64 {
-        match self {
-            Self::Vec(v) => v.position(),
-            Self::Cxx(cxx) => cxx.position(),
-        }
-    }
-    fn get_ref(&self) -> &[u8] {
-        match self {
-            Self::Vec(v) => v.get_ref(),
-            Self::Cxx(cxx) => cxx.get_ref().as_ref(),
-        }
-    }
-}
-
-struct FileLoader {
-    last_buffer: Option<Buffer>,
-}
-impl FileLoader {
-    fn get_file(&mut self, path: &Path, manager: AssetManager) -> Option<Buffer> {
-        let stripped = path.strip_prefix("assets/").unwrap_or(path);
-        if let Some(mut cache) = self.last_buffer.take_if(|c| c.name == path) {
-            log::info!("Cache hit!: {:#?}", path);
-            cache
-                .rewind()
-                .expect("Unable to rewind in a memory buffer?, impossible");
-            return Some(cache);
-        }
-        let replacement_list = folder_list! {
-            apk: "gui/dist/hbui/" -> pack: "hbui/",
-            apk: "skin_packs/persona/" -> pack: "persona/",
-            apk: "renderer/" -> pack: "renderer/",
-            apk: "resource_packs/vanilla/cameras/" -> pack: "vanilla_cameras/",
-        };
-        for replacement in replacement_list {
-            // Remove the prefix we want to change
-            if let Ok(file) = stripped.strip_prefix(replacement.0) {
-                let mut cxx_storage = StackString::new();
-                let mut cxx_ptr = unsafe { cxx_storage.init("") };
-                let Some(loadfn) = crate::RPM_LOAD.get() else {
-                    log::warn!("ResourcePackManager fn is not ready yet?");
-                    return None;
-                };
-                let mut resource_loc = ResourceLocation::new();
-                let mut cpppath = ResourceLocation::get_path(&mut resource_loc);
-                opt_path_join(cpppath.as_mut(), &[Path::new(replacement.1), file]);
-                let packm_ptr = crate::PACKM_OBJ.load(Ordering::Acquire);
-                if packm_ptr.is_null() {
-                    log::error!("ResourcePackManager ptr is null");
-                    return None;
-                }
-                unsafe {
-                    loadfn(packm_ptr, resource_loc, cxx_ptr.as_mut());
-                }
-                if cxx_ptr.is_empty() {
-                    log::info!("Cannot find file: {}", cpppath.as_ref());
-                    return None;
-                }
-                log::info!("Loaded ResourcePack file: {}", cpppath.as_ref());
-                let buffer = if file
-                    .as_os_str()
-                    .as_encoded_bytes()
-                    .ends_with(b".material.bin")
-                {
-                    match process_material(manager, cxx_ptr.as_bytes()) {
-                        Some(updated) => BufferCursor::Vec(Cursor::new(updated)),
-                        None => BufferCursor::Cxx(Cursor::new(cxx_storage)),
-                    }
-                } else {
-                    BufferCursor::Cxx(Cursor::new(cxx_storage))
-                };
-                let cache = Buffer::new(path.to_path_buf(), buffer);
-                // ResourceLocation gets dropped (also cxx_storage if its not needed)
-                return Some(cache);
-            }
-        }
-        None
-    }
-}
-struct Buffer {
-    name: PathBuf,
-    object: BufferCursor,
-}
-impl Buffer {
-    fn new(name: PathBuf, object: BufferCursor) -> Self {
-        Self { name, object }
-    }
-}
-impl Deref for Buffer {
-    type Target = BufferCursor;
-    fn deref(&self) -> &Self::Target {
-        &self.object
-    }
-}
-impl DerefMut for Buffer {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.object
-    }
-}
 trait AsCResult<T> {
     fn to_c_result(&self) -> T;
 }
