0ec03f0ef9fc68b632858c1f89b683e8b66aafe3
mcbegamerxx954 <154642722+mcbegamerxx954@users.noreply.github.com>
2025-12-15 21:34:53 -0300

    Slight optimizations

--- assets/lightmapUtil_10023_13028.glsl
+++ assets/lightmapUtil_10023_13028.glsl
@@ -1,8 +1,9 @@
 
 vec2 lightmapUtil_10023_13028_190d99(vec2 tc1){
+    uint tmp = uint(round(tc1.y * 65535.0));
     return clamp(vec2(uvec2(
-        uint(round(tc1.y * 65535.0)) >> 4u,
-        uint(round(tc1.y * 65535.0)) & 15u
+        tmp >> 4u,
+        tmp & 15u
     ) & 15u) * 0.066666, 0.0, 1.0);
 }
 #ifdef a_texcoord1
